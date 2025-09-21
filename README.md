8d7fe13a8dc6d2f69c15656ae2bedb24049f677e
Mazi <mgaray@comunidad.unnoba.edu.ar>
2025-09-21 15:29:45 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # poo2025-Pergamino-WizardProgrammers
 POO 2025-Pergamino-WizardProgrammers
+Este seria el tp2 de POO, que consistia en hacer un repositorio en github y clonarlo
---
+++ src/test/java/unnoba/WizardProgrammers/Tp2/Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package unnoba.WizardProgrammers.Tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
