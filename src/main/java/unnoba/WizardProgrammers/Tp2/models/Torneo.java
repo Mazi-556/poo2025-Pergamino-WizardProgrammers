4c6446054f2eb02f793b2e6625089444b6d4821b
Mazi <mgaray@comunidad.unnoba.edu.ar>
2025-10-11 14:49:35 -0300

    Anotar JPA-Entidades

--- src/main/java/unnoba/WizardProgrammers/Tp2/models/Torneo.java
+++ src/main/java/unnoba/WizardProgrammers/Tp2/models/Torneo.java
@@ -4,9 +4,14 @@ import lombok.Setter;
 import lombok.AllArgsConstructor;
 import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Torneo {
 
     private String nombre;
@@ -14,4 +19,8 @@ public class Torneo {
     private Date fechaInicio;
     private Date fechaFin;
     private boolean activo;
+
+    @ManyToOne
+    @JoinColumn(name="administrador_id", nullable=false)
+    private Administrador administrador_id;
 }
