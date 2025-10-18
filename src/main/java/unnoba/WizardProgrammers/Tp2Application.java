ab4e7c8af040990ca05bfb77cbf6adf418debb29
Facu <fdmoreno@comunidad.unnoba.edu.ar>
2025-10-18 15:13:03 -0300

    Cambio el main para poder ejecutar el codigo, y cambio hibernate de 'create' a 'update'

--- src/main/java/unnoba/WizardProgrammers/Tp2Application.java
+++ src/main/java/unnoba/WizardProgrammers/Tp2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Tp2Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Esta linea inicia la app
+        SpringApplication.run(Tp2Application.class, args); 
     }
 }
