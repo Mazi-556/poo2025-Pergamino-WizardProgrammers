72718dbd4d23bac83af077d179ee924289e7204e
Mazi <mgaray@comunidad.unnoba.edu.ar>
2026-01-21 13:56:56 -0300

    Agregue la pagina del login
    Pude conectar el front con el back (o eso espero). Hice un admin con email admin1@admin.com y contra admin y me dio un token, pero este solo aparece en los logs del navegador. Despues nos la vamos a tener que arreglar con automatizar esto (No tengo idea como hacerlo)

--- src/main/java/ar/edu/unnoba/poo2025/torneos/config/WebConfig.java
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/config/WebConfig.java
@@ -3,6 +3,7 @@ package ar.edu.unnoba.poo2025.torneos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,13 +12,18 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                // Configuramos CORS para permitir todo
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Permitir desde cualquier origen
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
+            }
+
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addRedirectViewController("/", "/login/index.html");
             }
         };
     }
